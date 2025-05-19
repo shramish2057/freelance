2cbfe896b3eeaea7a29719595d592da2ea6fdcdd
Shramish KSB <shramish.kafle@ksb.com>
2025-05-19 14:07:29 +0200

    feat: push freelance website

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Add this line to ensure correct path resolution
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
